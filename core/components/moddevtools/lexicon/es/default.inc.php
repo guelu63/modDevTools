189e2474b9d4c4b0da7d4343c0e9f5bda8f51f43
Abel Martínez <guelu63@gmail.com>
2018-05-16 12:12:32 +0200

    core/components/moddevtools/lexicon/es/default.inc.php
    Spanish lexicon.
    ;)

---
+++ core/components/moddevtools/lexicon/es/default.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+$_lang['moddevtools'] = 'modDevTools';
+
+$_lang['moddevtools_menu_desc'] = 'Manejo cómodo de elementos';
+
+$_lang['moddevtools_chunks_intro'] = 'Puedes editar directamente los chunks contenidos en la plantilla.';
+
+$_lang['moddevtools_snippets_intro'] = 'Puedes editar directamente los snippets contenidos en la plantilla.';
+
+$_lang['moddevtools_templates_intro'] = 'Puedes editar directamente la plantilla que contiene este chunk';
+
+$_lang['moddevtools_res_template_intro'] = 'La tabla muestra los recursos que usan estea plantilla.';
+
+$_lang['moddevtools_res_chunk_intro'] = 'La tabla muestra los recursos que contienen este chunk en su contenido o en su plantilla.';
+
+$_lang['moddevtools_res_snippet_intro'] = 'La tabla muestra los recursos que contienen este snippet en su contenido o en su plantilla.';
+
+$_lang['moddevtools_grid_actions'] = 'Acciones';
+
+$_lang['moddevtools_template_change'] = 'Cambiar plantilla';
+
+$_lang['moddevtools_search_desc'] = 'Buscar y reemplazar cadenas en plantillas y chunks.';
+
+$_lang['moddevtools_text_to_find'] = 'Texto a buscar';
+
+$_lang['moddevtools_replace'] = 'Reemplazar';
+
+$_lang['moddevtools_replace_all'] = 'Reemplazar todo';
+
+$_lang['moddevtools_replace_with'] = 'Reemplazar con';
+
+$_lang['moddevtools_find'] = 'Encontrar';
+
+$_lang['moddevtools_skip'] = 'Saltar';
+
+$_lang['moddevtools_notfound'] = '¡No se puede emncontrar el texto';
+
+$_lang['moddevtools_search_filters'] = 'Buscar solamente en elementos';
