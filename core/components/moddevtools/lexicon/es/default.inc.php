bc3641ba1638ada2aa235c77644cd28f1dc95d49
Abel Martínez <guelu63@gmail.com>
2020-08-25 10:58:42 +0200

    translate

---
+++ core/components/moddevtools/lexicon/es/default.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+$_lang['moddevtools'] = 'modDevTools';
+$_lang['moddevtools_menu_desc'] = 'Manejo cómodo de elementos';
+$_lang['moddevtools_chunks_intro'] = 'Puedes editar directamente los chunks contenidos en la plantilla.';
+$_lang['moddevtools_snippets_intro'] = 'Puedes editar directamente los snippets contenidos en la plantilla.';
+$_lang['moddevtools_templates_intro'] = 'Puedes editar directamente las plantillas que contengan este chunk.';
+$_lang['moddevtools_res_template_intro'] = 'La tabla muestra los recursos que usan esta plantilla.';
+$_lang['moddevtools_res_chunk_intro'] = 'La tabla muestra los recursos que contienen este chunk en el contenido o en la plantilla.';
+$_lang['moddevtools_res_snippet_intro'] = 'La tabla muestra los recursos que contienen este snippet en el contenido o en la plantilla.';
+$_lang['moddevtools_grid_actions'] = 'Acciones';
+$_lang['moddevtools_template_change'] = 'Cambiar plantilla';
+$_lang['moddevtools_search_desc'] = 'Busca y reemplaza cadenas en plantillas y chunks.';
+$_lang['moddevtools_text_to_find'] = 'Texto a buscar';
+$_lang['moddevtools_replace'] = 'Remplazar';
+$_lang['moddevtools_replace_all'] = 'Remplazar todo';
+$_lang['moddevtools_replace_with'] = 'Remplazar con';
+$_lang['moddevtools_find'] = 'Encontrar';
+$_lang['moddevtools_skip'] = 'Omitir';
+$_lang['moddevtools_notfound'] = '¡No se puede encontrar el texo!';
+$_lang['moddevtools_search_filters'] = 'Buscar solo en elementos';
